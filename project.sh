239ddf5d77baeba4ce2ef79253ed3dd7e7008a97
kindasorrow <whats_good@mail.ru>
2025-06-29 14:38:56 +0500

    add README and Swagger documentation for Company Fleet API

--- project.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker compose up -d --build
-docker compose exec php php artisan key:generate
-docker compose exec php php artisan migrate --seed
-docker compose exec php php artisan config:cache
-
-echo -e "\n✅  Application is running at http://localhost:${WEB_PORT:-8080}\n"
